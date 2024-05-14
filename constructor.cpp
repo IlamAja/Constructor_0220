b3f45acbbace8b2f6b9a2575f8b2a6a1d66eee7d
Ilam Ball <informasiilam@gmail.com>
2024-05-14 18:22:01 +0700

    memperbaiki codingan constructor

--- constructor.cpp
+++ constructor.cpp
@@ -4,33 +4,53 @@ using namespace std;
 
 class mahasiswa
 {
-    public:
+    private:
         int nim;
         string nama;
      
      public:
-        mahasiswa()
+        mahasiswa();
+        mahasiswa(int);
+        mahasiswa(string);
+        mahasiswa(int iNim, string iNama);
+        void cetak();
+};
+        mahasiswa::mahasiswa()
         {
             nim = 0;
             nama = "";
         }
-        mahasiswa(int iNim)
+        mahasiswa::mahasiswa(int iNim)
         {
             nim = iNim;
         };
-        mahasiswa(int iNim, string iNama)
+        mahasiswa::mahasiswa(string iNama)
+        {
+            nama = iNama;
+        };
+        mahasiswa::mahasiswa(int iNim, string iNama)
         {
             nim = iNim;
             nama = iNama;
         };
-        void cetak(){
+
+        void mahasiswa::cetak(){
             cout << "NIM = " << nim << endl;
             cout << "Nama = " << nama << endl;
             cout << endl;
         };
-};
 
 int main()
 {
-    
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
 }
