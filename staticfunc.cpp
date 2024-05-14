848b4240108560916b821ecad226e4220bd10793
Ilam Ball <informasiilam@gmail.com>
2024-05-14 14:55:34 +0700

    membuat class mahasiswa dan membernya

--- staticfunc.cpp
+++ staticfunc.cpp
@@ -2,3 +2,15 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+    private:
+        static int nim;
+    
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+};
